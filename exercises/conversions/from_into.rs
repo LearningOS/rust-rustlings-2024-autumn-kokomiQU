0a323b6978c7f66cb3a52dbcaf6804d7079202a7
u202114824 <u202114824@hust.edu.cn>
2024-10-07 22:47:01 +0800

    1

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut parts = s.split(',');
+        let name = parts.next().unwrap_or_default();
+        let age = parts.next().unwrap_or_default();
+        if parts.next() != None {
+            return Person::default();
+        }
+        if name.is_empty() {
+            return Person::default();
+        }
+        match age.parse::<usize>() {
+            Ok(age) => Person { name: name.to_string(), age },
+            Err(_) => Person::default(),
+        }
+        
+
+
+
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let mut iter = s.split(',');
+        let name = iter.next().ok_or(ParsePersonError::NoName)?;
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = iter.next().ok_or(ParsePersonError::BadLen)?;
+        let age = age.parse::<usize>().map_err(ParsePersonError::ParseInt)?;
+        if iter.next().is_some() {
+            return Err(ParsePersonError::BadLen);
+        }
+        Ok(Person { name: name.to_string(), age })
+
     }
 }
 
